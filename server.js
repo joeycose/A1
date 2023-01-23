bd664b634342f380ff0f73bbd24cc392ac8ec145
gcosentino1 <battlefitbodywear@outlook.com>
2023-01-22 23:17:52 -0500

    new file:   .env 	new file:   .gitignore

---
+++ server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const MoviesDB = require("./modules/moviesDB.js")
+const db = new MoviesDB();
+
+// port number that our app is running on
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API Listening' })
+});
+
+
+db.initialize(process.env.MONGODB_CONN_STRING).then(() => {
+    app.listen(HTTP_PORT, () => {
+        console.log(`server listening on : ${HTTP_PORT}`);
+    });
+}).catch((err) => {
+    console.log(err);
+});
+
+// POST /api/movies
+app.post("/api/movies", async (req, res) => {
+    try {
+        const newMovie = await db.addNewMovie(req.body);
+        return res.status(201).json(newMovie);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+// GET /api/movies
+app.get("/api/movies", async (req, res) => {
+    const { page, perPage, title } = req.query;
+
+    try {
+        const movies = await db.getAllMovies(page, perPage, title);
+        return res.status(200).json(movies);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+// GET /api/movies/:id
+app.get("/api/movies/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const movie = await db.getMovieById(id);
+        return res.status(200).json(movie);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+// PUT /api/movies/:id
+app.put("/api/movies/:id", async (req, res) => {
+    const { id } = req.params;
+    const data = req.body;
+
+    try {
+        const updatedMovie = await db.updateMovieById(data, id);
+        return res.status(204).json(updatedMovie);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+// DELETE /api/movies/:id
+app.delete("/api/movies/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletedMovie = await db.deleteMovieById(id);
+        return res.status(204).json(deletedMovie);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+// app.listen(HTTP_PORT, () => {
+//     console.log('Ready to handle requests on port ' + HTTP_PORT)
+// });
+
