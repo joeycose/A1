b7d94fd3d33a63f6b82342716a1896c7e307ebcd
Giuseppe Cosentino <85801475+joeycose@users.noreply.github.com>
2023-02-19 13:26:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-/*********************************************************************************
-* BTI425 – Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: Giuseppe Cosentino Student ID: ______________ Date: Sun, Jan 22
-* Cyclic Link: https://fair-teal-eel-hose.cyclic.app/
-*
-********************************************************************************/ 
-
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
